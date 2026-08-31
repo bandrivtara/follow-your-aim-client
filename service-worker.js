96994b1bfe35e0242fc388cac11dcc800c7832c3
Taras Bandrivskyy <tb.js.dev@gmail.com>
2026-08-31 17:56:10 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "follow-your-aim-shell-v1";
+const CACHE_NAME = "follow-your-aim-shell-v2";
 const APP_SHELL = ["./", "./index.html", "./manifest.json", "./logo192.png"];
 
 self.addEventListener("install", (event) => {
